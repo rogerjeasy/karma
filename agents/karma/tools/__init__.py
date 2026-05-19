ab227692b9aaac0303efc0ac967ddfe0a37d1cfd
rogerjeasy <rogerjeasy@gmail.com>
2026-05-19 22:20:42 +0200

    fix agents CI: use uv to resolve deep dependency graph

--- agents/karma/tools/__init__.py
+++ agents/karma/tools/__init__.py
@@ -1,15 +1,3 @@
 from karma.tools.contract_validator import ContractValidator
-from karma.tools.dynatrace_events import emit_karma_event
-from karma.tools.dynatrace_mcp import (
-    ALL_KNOWN_TOOLS,
-    REQUIRED_TOOLS,
-    build_dynatrace_toolset,
-)
 
-__all__ = [
-    "build_dynatrace_toolset",
-    "emit_karma_event",
-    "ContractValidator",
-    "ALL_KNOWN_TOOLS",
-    "REQUIRED_TOOLS",
-]
+__all__ = ["ContractValidator"]
